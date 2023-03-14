fbaff148798e0b26032ee2a15c5d171a1e17d93a
AnoukW01 <wittemananouk@gmail.com>
2023-03-14 21:04:18 +0100

    Mapping libraries

--- js/nieuw-zeeland.js
+++ js/nieuw-zeeland.js
@@ -1,14 +1,38 @@
 //OpenLayers kaart//
 const openLayerMap = new ol.Map({
-    target: 'openlayermap',
+    target: 'openLayerMap',
     layers: [
         new ol.layer.Tile({
-            source: new ()
+            source: new ol.source.OSM()
         })
     ],
     view: new ol.View({
-        center: ol.proj.
-         fromLonLat([-36.8862835, 174.5322907]),
+        center: ol.proj.fromLonLat([174.5322907, -36.8862835]),
         zoom: 8
     })
 });
+
+//ArcGIS kaart//
+require(["esri/config", "esri/Map", "esri/views/MapView"], function (esriConfig, Map, MapView) {
+
+	esriConfig.apiKey = "AAPK6d1c65e750a94a5da99311d143af405cC8s5_iv42bD6dQ_GlPgL1SJDBrrJTvQX5_t_9KaoEsAsRMM5TrU0g5tESjukBIrX";
+
+	const arcGisMap = new Map({
+		basemap: "arcgis-topographic" 
+	});
+
+	const View = new MapView({
+		map: arcGisMap,
+		center: [174.5322907, -36.8862835], 
+		zoom: 8, 
+		container: "arcGisMap",
+	});
+});
+
+//Maplibre kaart//
+var mapLibre = new maplibregl.Map({
+    container: 'mapLibre',
+    style: 'https://demotiles.maplibre.org/style.json', // stylesheet location
+    center: [174.5322907, -36.8862835], // starting position [lng, lat]
+    zoom: 9 // starting zoom
+    });
--- js/zuid-korea.js
+++ js/zuid-korea.js
@@ -1,10 +1,10 @@
 //Leaflet kaart//
-const leafletmap = L.map('leafletmap').setView([37.566535, 126.9779692], 15);
+const leafLet = L.map('leafLet').setView([37.566535, 126.9779692], 15);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-    }).addTo(leafletmap);
+    }).addTo(leafLet);
 
-L.marker([37.566535, 126.9779692]).addTo(leafletmap)
+L.marker([37.566535, 126.9779692]).addTo(leafLet)
 .bindPopup('SEOUL')
 .openPopup();
