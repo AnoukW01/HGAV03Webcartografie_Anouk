71173af8eec6d1cb58192f892327c2c1c55746b1
AnoukW01 <wittemananouk@gmail.com>
2023-03-28 21:41:05 +0200

    leaflet geojson

--- js/zuid-korea.js
+++ js/zuid-korea.js
@@ -5,16 +5,18 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     }).addTo(leafLet);
 
-L.marker([37.566535, 126.9779692]).addTo(leafLet)
-   .bindPopup('SEOUL')
-   .openPopup();
 
+L.marker([37.566535, 126.9779692]).addTo(leafLet)
+  .bindPopup('SEOUL')
+  .openPopup();
 
 let firstAPIrequist = L.geoJSON().addTo(leafLet); 
 fetch('../data/southkorea.geojson') 
   .then(response => response.json()) 
   .then(data => 
     { console.log(data);
-        firstAPIrequist.addData(data)
+      firstAPIrequist.addData(data)
+      L.Control.geocoder().addTo(leafLet)
      })
+
 
