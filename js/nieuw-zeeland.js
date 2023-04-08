8a7af30ac2b461d09f7455ef9e23ae759ba78000
AnoukW01 <wittemananouk@gmail.com>
2023-04-08 19:58:42 +0200

    Javascript + tekst toevoegen
    Javascript elementen + begeleidende teksten toegevoegd

--- js/nieuw-zeeland.js
+++ js/nieuw-zeeland.js
@@ -9,21 +9,27 @@ const openLayerMap = new ol.Map({
             source: new ol.source.Vector({
                 url: '../data/data.json',
                 format: new ol.format.GeoJSON()
-            })
+            }),
         })
     ],
     view: new ol.View({
-        center: ol.proj.fromLonLat([174.5322907, -36.8862835]),
-        zoom: 8
+        center: ol.proj.fromLonLat([175.7166638, -38.3999984]),
+        zoom: 6
     })
 })
 
+var zoomslider = new ol.control.ZoomSlider()
+openLayerMap.addControl(zoomslider)
+
+var scale = new ol.control.ScaleLine()
+openLayerMap.addControl(scale)
+
 //Leaflet kaart//
-const leafLet = L.map('leafLetTracks').setView([-36.8862835, 174.5322907], 6);
+const leafLet = L.map('leafLetTracks').setView([-43.3744881, 172.4662705], 5);
 
 L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/Specialty/DeLorme_World_Base_Map/MapServer/tile/{z}/{y}/{x}', {
-        attribution: 'Tiles &copy; Esri &mdash; Copyright: &copy;2012 DeLorme'
-    }).addTo(leafLet);
+    attribution: 'Tiles &copy; Esri &mdash; Copyright: &copy;2012 DeLorme',
+}).addTo(leafLet);
 
 L.tileLayer.wms('http://localhost:8001/geoserver/ows', {
     'layers': 'aeres_webcartografie:doc_tracks',
@@ -35,35 +41,49 @@ L.tileLayer.wms('http://localhost:8001/geoserver/ows', {
 }).addTo(leafLet)
 
 //ArcGIS kaart// 
-require(["esri/config", "esri/WebMap", "esri/views/MapView", "esri/widgets/ScaleBar", "esri/widgets/Legend", "esri/widgets/Legend"], function(esriConfig, WebMap, MapView, ScaleBar, Legend) {
+require(["esri/config", "esri/WebMap", "esri/views/MapView", "esri/widgets/ScaleBar", "esri/widgets/Legend", "esri/widgets/Legend"], function (esriConfig, WebMap, MapView, ScaleBar, Legend) {
 
-esriConfig.apiKey = "AAPK6d1c65e750a94a5da99311d143af405cC8s5_iv42bD6dQ_GlPgL1SJDBrrJTvQX5_t_9KaoEsAsRMM5TrU0g5tESjukBIrX";
-    
-const arcGisMap = new WebMap({
-    portalItem: {
-        id: "86848bf24c754eff94c2d455d309f990"
-    }
+    esriConfig.apiKey = "AAPK6d1c65e750a94a5da99311d143af405cC8s5_iv42bD6dQ_GlPgL1SJDBrrJTvQX5_t_9KaoEsAsRMM5TrU0g5tESjukBIrX";
+
+    const arcGisMap = new WebMap({
+        portalItem: {
+            id: "86848bf24c754eff94c2d455d309f990"
+        }
+    });
+
+    const view = new MapView({
+        container: "arcGisMap",
+        map: arcGisMap,
+        zoom: 4,
+    });
+
+    // Legenda
+    const legend = new Legend({
+        view: view
+    });
+
+    // Legenda toevoegen ArcGIS kaart
+    view.ui.add(legend, "bottom-left");
+
+    view.on("click", function(event){
+        // event is the event handle returned after the event fires.
+        console.log(event.mapPoint);
+      });
 });
 
-const view = new MapView({
-    container: "arcGisMap",
-    map: arcGisMap,
-    zoom: 8,
-});
-
-// Legenda
-const legend = new Legend({
-    view: view
-});
-
-// Legenda toevoegen ArcGIS kaart
-view.ui.add(legend, "bottom-left");
-}); 
 
 //Maplibre kaart//
 var mapLibre = new maplibregl.Map({
     container: 'mapLibre',
-    style: 'https://demotiles.maplibre.org/style.json', // stylesheet location
-    center: [169.97533871680452, -43.69689631783225], // starting position [lng, lat]
-    zoom: 9 // starting zoom
-    });
+    style: 'https://api.maptiler.com/maps/topo-v2/style.json?key=yGJLWGQTJVAjcW7WkmGz', // stylesheet location
+    center: [175.7166638, -38.3999984], // starting position [lng, lat]
+    zoom: 5// starting zoom
+});
+
+const marker = new maplibregl.Marker({
+    color: '#a66034'
+})
+
+    .setLngLat([174.763336, -36.848461])
+    .setPopup(new maplibregl.Popup().setHTML("<h3>Hi, visit Auckland!</h3>")) // add popup
+    .addTo(mapLibre)
--- js/zuid-korea.js
+++ js/zuid-korea.js
@@ -1,20 +1,22 @@
 //Leaflet kaart//
-const leafLet = L.map('leafLet').setView([37.80369867390229, 126.98105880412463], 6);
+const leafLet = L.map('leafLet').setView([35.907757, 127.766922], 6);
+leafLet.zoomControl.setPosition('bottomleft');
 
 L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/Specialty/DeLorme_World_Base_Map/MapServer/tile/{z}/{y}/{x}', {
-        attribution: 'Tiles &copy; Esri &mdash; Copyright: &copy;2012 DeLorme'
-    }).addTo(leafLet);
-
+  attribution: 'Tiles &copy; Esri &mdash; Copyright: &copy;2012 DeLorme',
+  minZoom: 6,
+  maxZoom: 10,
+}).addTo(leafLet);
 
 L.marker([37.566535, 126.9779692]).addTo(leafLet)
   .bindPopup('SEOUL')
   .openPopup();
 
-let firstAPIrequest = L.geoJSON().addTo(leafLet); 
+let firstAPIrequest = L.geoJSON().addTo(leafLet);
 fetch("https://raw.githubusercontent.com/AnoukW01/HGAV03Webcartografie_Anouk/main/data/southkorea.geojson", {})
   .then(response => response.json())
-  .then(data => { 
-      console.log(data);
-      firstAPIrequest.addData(data)
-      L.Control.geocoder().addTo(leafLet)  
+  .then(data => {
+    console.log(data);
+    firstAPIrequest.addData(data)
+    L.Control.geocoder().addTo(leafLet)
   });
